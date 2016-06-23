011b5ced06d89c21e2ea00adaec4d72cd20243c7
Andy Hanson <anhans@microsoft.com>
2016-06-23 07:12:37 -0700

    Support "not needed" packages

--- src/generate-packages.ts
+++ src/generate-packages.ts
@@ -1,17 +1,26 @@
+import * as fs from 'fs'
 import * as common from './lib/common';
 import * as generator from './lib/package-generator';
 
 const typeData = <common.TypesDataFile>common.readDataFile(common.typesDataFilename);
 
 if (typeData === undefined) {
-	console.log('Run parse-definitions first!');
-} else {
-	const log: string[] = [];
-	Object.keys(typeData).forEach(packageName => {
-		const typing = typeData[packageName];
-		const result = generator.generatePackage(typing, typeData);
-		log.push(` * ${packageName}`);
-		result.log.forEach(line => log.push(`   * ${line}`));
-	});
-	common.writeLogSync('package-generator.md', log);
+	throw new Error('Run parse-definitions first!');
+}
+
+const log: string[] = [];
+Object.keys(typeData).forEach(packageName => {
+	const typing = typeData[packageName];
+	logGeneration(typing, generator.generatePackage(typing, typeData));
+});
+
+for (const pkg of common.readNotNeededPackages()) {
+	logGeneration(pkg, generator.generateNotNeededPackage(pkg));
+}
+
+common.writeLogSync('package-generator.md', log);
+
+function logGeneration(pkg: common.AnyPackage, generateResult: { log: string[] }): void {
+	log.push(` * ${pkg.libraryName}`);
+	generateResult.log.forEach(line => log.push(`   * ${line}`));
 }
