df8596745f99a9e03b4d60f5fe07f07d6e926c7e
Andy Hanson <anhans@microsoft.com>
2016-07-01 06:42:27 -0700

    Remove `request`, which has been replaced by `fetch`.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,7 +1,6 @@
 
 /// <reference path="node/node.d.ts" />
 /// <reference path="typescript/typescript.d.ts" />
-/// <reference path="request/request.d.ts" />
 /// <reference path="yargs/yargs.d.ts" />
 /// <reference path="source-map-support/source-map-support.d.ts" />
 /// <reference path="node-fetch/node-fetch.d.ts" />
