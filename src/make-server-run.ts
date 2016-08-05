a507c2dc45204d97afbdd001c87602041c8e0afd
Andy Hanson <anhans@microsoft.com>
2016-08-05 08:02:02 -0700

    Ensure webhook collects *all* request data.

--- src/make-server-run.ts
+++ src/make-server-run.ts
@@ -7,15 +7,15 @@ import * as yargs from "yargs";
 if (!module.parent) {
 	const remote = yargs.argv.remote;
 	const key = process.env["GITHUB_SECRET"];
-	if (!key) {
-		throw new Error("Must provide GITHUB_SECRET");
+	const port = parseInt(process.env["PORT"], 10);
+	if (!(key && port)) {
+		throw new Error("Must provide GITHUB_SECRET and PORT");
 	}
-	done(main(key, remote));
+	done(main(key, port, remote));
 }
 
-async function main(key: string, remote: boolean): Promise<void> {
-	// lvh.me reroutes to localhost
-	const url = remote ? "http://types-publisher.azurewebsites.net" : "http://lvh.me";
+async function main(key: string, port: number, remote: boolean): Promise<void> {
+	const url = remote ? "http://types-publisher.azurewebsites.net" : `http://localhost:${port}`;
 	const body = JSON.stringify({ ref: `refs/heads/${settings.sourceBranch}` });
 	const headers = { "x-hub-signature": expectedSignature(key, body) };
 	const resp = await fetch(url, { method: "POST", body, headers });
