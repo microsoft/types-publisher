632fa85051abb740930d61aea0ce7d281090d9fd
Andy Hanson <anhans@microsoft.com>
2016-08-22 13:55:30 -0700

    Add script to fix all packages whose README has not been uploaded

--- src/calculate-versions.ts
+++ src/calculate-versions.ts
@@ -1,5 +1,6 @@
+import * as child_process from "child_process";
 import * as yargs from "yargs";
-import { existsTypesDataFileSync, readTypings } from "./lib/common";
+import { existsTypesDataFileSync, fullPackageName, readTypings } from "./lib/common";
 import Versions, { Changes, writeChanges } from "./lib/versions";
 import { done } from "./lib/util";
 
@@ -8,7 +9,8 @@ if (!module.parent) {
 		console.log("Run parse-definitions first!");
 	} else {
 		const forceUpdate = yargs.argv.forceUpdate;
-		done(main(forceUpdate));
+		//done(main(forceUpdate));
+		done(fixNotReadme());
 	}
 }
 
@@ -24,4 +26,37 @@ export default async function main(forceUpdate: boolean): Promise<void> {
 	}
 	await versions.saveLocally();
 	await writeChanges(changes);
+}
+
+export async function fixNotReadme() {
+	const versions = await Versions.loadFromBlob();
+	const changes: Changes = [];
+
+	for (const typing of await readTypings()) {
+		console.log(typing.typingsPackageName);
+		if (!(await hasReadme(typing.typingsPackageName))) {
+			versions.recordUpdate(typing, /*forceUpdate*/true);
+			console.log(`Force update of ${typing.typingsPackageName}, which has no README`);
+		}
+	}
+
+	await versions.saveLocally();
+	await writeChanges(changes);
+}
+
+async function hasReadme(packageName: string): Promise<boolean> {
+	return (await fetchReadme(packageName)).trim().length > 0;
+}
+
+async function fetchReadme(packageName: string): Promise<string> {
+	return new Promise<string>((resolve, reject) => {
+		child_process.exec(`npm info ${fullPackageName(packageName)} readme`, { encoding: "utf8" }, (err, stdout) => {
+			if (err) {
+				reject(err);
+			}
+			else {
+				resolve(stdout);
+			}
+		});
+	});
 }
