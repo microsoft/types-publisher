83a6b5422fbb83f6d8d17d0d504810e1861461c7
Andy Hanson <anhans@microsoft.com>
2016-11-28 09:52:14 -0800

    error.stack may be undefined, so fallback to error.message

--- src/util/util.ts
+++ src/util/util.ts
@@ -127,3 +127,7 @@ export async function execAndThrowErrors(cmd: string, cwd?: string): Promise<str
 export function skipBOM(str: string): string {
 	return str.charCodeAt(0) === 0xFEFF ? str.slice(1) : str;
 }
+
+export function errorDetails(error: Error): string {
+	return error.stack || error.message;
+}
