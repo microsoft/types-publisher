666a71861546152c3b1eeaa5a1109196f7d0621c
Andy Hanson <anhans@microsoft.com>
2016-12-08 10:20:53 -0800

    Add lint rule: Only allow `void` as return type

---
+++ src/tslint/voidReturnRule.ts
@@ -0,0 +1,51 @@
+import * as Lint from "tslint";
+import * as ts from "typescript";
+
+export class Rule extends Lint.Rules.AbstractRule {
+	static metadata: Lint.IRuleMetadata = {
+		ruleName: "void-return",
+		description: "`void` may only be used as a return type.",
+		rationale: "style",
+		optionsDescription: "Not configurable.",
+		options: null,
+		type: "style",
+		typescriptOnly: true,
+	};
+
+	static FAILURE_STRING = "Use the `void` type for return types only. Otherwise, use `undefined`.";
+
+	apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+		return this.applyWithWalker(new Walker(sourceFile, this.getOptions()));
+	}
+}
+
+class Walker extends Lint.RuleWalker {
+    visitNode(node: ts.Node) {
+        if (node.kind === ts.SyntaxKind.VoidKeyword) {
+            //todo: isFunctionLike...
+            const parent = node.parent!;
+            if (!(isSignatureDeclaration(parent) && parent.type === node)) {
+                this.fail(node, Rule.FAILURE_STRING);
+            }
+        }
+        super.visitNode(node);
+    }
+
+    private fail(node: ts.Node, message: string) {
+		this.addFailure(this.createFailure(node.getStart(), node.getWidth(), message));
+	}
+}
+
+function isSignatureDeclaration(node: ts.Node): node is ts.SignatureDeclaration {
+    switch (node.kind) {
+        case ts.SyntaxKind.ArrowFunction:
+        case ts.SyntaxKind.FunctionDeclaration:
+        case ts.SyntaxKind.FunctionType:
+        case ts.SyntaxKind.MethodDeclaration:
+        case ts.SyntaxKind.MethodSignature:
+            return true;
+
+        default:
+            return false;
+    }
+}
