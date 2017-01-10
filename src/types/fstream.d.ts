74df0dc7d3482178cc695bf1448c268675fcef45
Andy Hanson <anhans@microsoft.com>
2017-01-10 13:29:50 -0800

    Improve typings in tgz

--- src/types/fstream.d.ts
+++ src/types/fstream.d.ts
@@ -1,1 +1,9 @@
-export function Reader(options: { path: string, type: "Directory" }): NodeJS.ReadableStream;
+export function Reader(options: ReaderOptions): NodeJS.ReadableStream;
+interface ReaderOptions {
+	path: string;
+	type: "Directory";
+	filter(entry: FStreamEntry): boolean;
+}
+interface FStreamEntry {
+	props: { type: string, mode: number };
+}
