725fe1928b6684cb73d3de1addb7bd4e9846d4f8
Andy Hanson <anhans@microsoft.com>
2017-02-15 13:44:59 -0800

    forbidden-types: For "Object", recommend "object"

--- src/tslint/forbiddenTypesRule.ts
+++ src/tslint/forbiddenTypesRule.ts
@@ -18,8 +18,6 @@ export class Rule extends Lint.Rules.AbstractRule {
 
 	static FUNCTION_FAILURE_STRING = "Avoid using the Function type. Prefer a specific function type, like `() => void`.";
 
-	static OBJECT_FAILURE_STRING = "Avoid using the Object type. Did you mean `any` or `{}`?";
-
 	apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
 		return this.applyWithWalker(new Walker(sourceFile, this.getOptions()));
 	}
@@ -40,9 +38,7 @@ function nameFailure(name: string): string | undefined {
 	switch (name) {
 		case "Function":
 			return Rule.FUNCTION_FAILURE_STRING;
-		case "Object":
-			return Rule.OBJECT_FAILURE_STRING;
-		case "Boolean": case "Number": case "String":
+		case "Boolean": case "Number": case "Object": case "String":
 			return Rule.upperCaseFailureString(name);
 		default:
 			return undefined;
