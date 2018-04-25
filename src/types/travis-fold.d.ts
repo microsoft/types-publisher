84f6aa13b35ff21690d85fe33a72d58f70449171
Andy Hanson <anhans@microsoft.com>
2018-04-25 15:39:11 -0700

    Use @types/travis-fold

--- src/types/travis-fold.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "travis-fold" {
-    export function start(group: string): string;
-    export function end(group: string): string;
-    export function wrap(group: string, content: string): string;
-    export function pushStart(ret: string[], group: string): void;
-    export function pushEnd(ret: string[], group: string): void;
-    export function isTravis(): boolean;
-}
