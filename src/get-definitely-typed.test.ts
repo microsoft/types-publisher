da6b6efad190bd04b3ff8d41f020ba71aeadeb0a
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-11-25 14:11:12 -0800

    Improved tester syntax

--- src/get-definitely-typed.test.ts
+++ src/get-definitely-typed.test.ts
@@ -1,3 +1,13 @@
-test('simple sum', () => {
-    expect(1 + 2).toBe(3);
-});
+function detest(o: { [s: string]: () => void }) {
+    for (const k in o) {
+        test(k, o[k])
+    }
+}
+detest({
+    simpleSum() {
+        expect(1 + 2).toBe(3);
+    },
+    ['even simpler sum']() {
+        expect(1 + 1).toBe(2);
+    }
+})
