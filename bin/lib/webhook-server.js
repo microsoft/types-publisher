48384652cf6df04119906dd5900075c8f2440e39
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-11-29 09:14:51 -0800

    Update bin/

--- bin/lib/webhook-server.js
+++ bin/lib/webhook-server.js
@@ -12,7 +12,13 @@ const rolling_logs_1 = require("./rolling-logs");
 const settings_1 = require("./settings");
 async function webhookServer(key, githubAccessToken, dry, fetcher, options) {
     return listenToGithub(key, githubAccessToken, fetcher, updateOneAtATime(async (log, timeStamp) => {
-        const github = new Github();
+        const github = new Github({
+            baseUrl: 'https://api.github.com',
+            headers: {
+                accept: 'application/vnd.github.v3+json',
+                'user-agent': `octokit/rest.js v16.1.0 Node.js/${process.version.substr(1)} (Windows; ${process.arch})`
+            }
+        });
         github.authenticate({
             type: "token",
             token: githubAccessToken
