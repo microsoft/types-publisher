2f40f1c956ff0e0b0072d0e24c076ad32cf0fc2e
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-01-31 10:52:24 -0800

    Merge webhook and webhook-server

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,11 +1,13 @@
 import * as yargs from "yargs";
-
+import { setInterval } from "timers";
 import appInsights = require("applicationinsights");
+
+import full from "./full";
 import { Options } from "./lib/common";
 import { getSecret, Secret } from "./lib/secrets";
-import webhookServer from "./lib/webhook-server";
+import { LoggerWithErrors, loggerWithErrors } from "./util/logging";
 import { Fetcher } from "./util/io";
-import { logUncaughtErrors } from "./util/util";
+import { logUncaughtErrors, currentTimeStamp } from "./util/util";
 
 if (!module.parent) {
     logUncaughtErrors(main());
@@ -36,4 +38,54 @@ export default async function main(): Promise<void> {
             throw e;
         }
     }
+}
+
+async function webhookServer(
+    githubAccessToken: string,
+    dry: boolean,
+    fetcher: Fetcher,
+    options: Options,
+): Promise<void> {
+    const fullOnce = updateOneAtATime(async (log) => {
+        const timeStamp = currentTimeStamp();
+        log.info(""); log.info("");
+        log.info(`# ${timeStamp}`);
+        log.info("");
+        log.info("Starting full...");
+        await full(dry, timeStamp, githubAccessToken, fetcher, options);
+    });
+
+    const log = loggerWithErrors()[0];
+    await fullOnce(log);
+    setInterval(fullOnce, 1_000_000, log);
+}
+
+// Even if there are many changes to DefinitelyTyped in a row, we only perform one update at a time.
+function updateOneAtATime(
+    doOnce: (log: LoggerWithErrors) => Promise<void>,
+): (log: LoggerWithErrors) => Promise<void> | undefined {
+    let working = false;
+    let anyUpdatesWhileWorking = false;
+
+    return (log) => {
+        if (working) {
+            anyUpdatesWhileWorking = true;
+            log.info("Not starting update, because already performing one.");
+            return undefined;
+        } else {
+            working = false;
+            anyUpdatesWhileWorking = false;
+            return work();
+        }
+
+        async function work(): Promise<void> {
+            log.info("Starting update");
+            working = true;
+            anyUpdatesWhileWorking = false;
+            do {
+                await doOnce(log);
+                working = false;
+            } while (anyUpdatesWhileWorking);
+        }
+    };
 }
