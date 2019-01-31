2a5699c2308f79d0c07c3bb12e80cc1161d6a9d4
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-01-31 10:53:37 -0800

    Remove webhook part 1 (#557)
    * Remove webhook, part 1
    
    Quick-and-dirty once again. I think this will work fine, but I need to
    test it before putting more effort in.
    
    * Use updateOneAtATime correctly, get rid of server
    
    Also don't need the PORT enviroment variable anymore.
    
    * Merge webhook and webhook-server

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,11 +1,13 @@
 import * as yargs from "yargs";
-
+import { setInterval } from "timers";
 import appInsights = require("applicationinsights");
+
+import full from "./full";
 import { Options } from "./lib/common";
 import { getSecret, Secret } from "./lib/secrets";
-import webhookServer from "./lib/webhook-server";
+import { LoggerWithErrors, loggerWithErrors } from "./util/logging";
 import { Fetcher } from "./util/io";
-import { logUncaughtErrors } from "./util/util";
+import { logUncaughtErrors, currentTimeStamp } from "./util/util";
 
 if (!module.parent) {
     logUncaughtErrors(main());
@@ -15,19 +17,16 @@ export default async function main(): Promise<void> {
     const key = await getSecret(Secret.GITHUB_SECRET);
     const githubAccessToken = await getSecret(Secret.GITHUB_ACCESS_TOKEN);
     const dry = !!(yargs.argv.dry || process.env.WEBHOOK_FORCE_DRY);
-    const port = process.env.PORT;
 
-    if (!(key && githubAccessToken && port)) {
-        console.log("The environment variables GITHUB_SECRET and GITHUB_ACCESS_TOKEN and PORT must be set.");
+    if (!(key && githubAccessToken)) {
+        console.log("The environment variables GITHUB_SECRET and GITHUB_ACCESS_TOKEN must be set.");
     } else {
         console.log(`=== ${dry ? "DRY" : "PRODUCTION"} RUN ===`);
         appInsights.setup(process.env["APPINSIGHTS_INSTRUMENTATIONKEY"]).start();
         console.log("Done initialising App Insights");
         const fetcher = new Fetcher();
         try {
-            const s = await webhookServer(key, githubAccessToken, dry, fetcher, Options.azure);
-            console.log(`Listening on port ${port}`);
-            s.listen(port);
+             await webhookServer(githubAccessToken, dry, fetcher, Options.azure);
         }
         catch (e) {
             appInsights.defaultClient.trackEvent({
@@ -39,4 +38,54 @@ export default async function main(): Promise<void> {
             throw e;
         }
     }
+}
+
+async function webhookServer(
+    githubAccessToken: string,
+    dry: boolean,
+    fetcher: Fetcher,
+    options: Options,
+): Promise<void> {
+    const fullOnce = updateOneAtATime(async (log) => {
+        const timeStamp = currentTimeStamp();
+        log.info(""); log.info("");
+        log.info(`# ${timeStamp}`);
+        log.info("");
+        log.info("Starting full...");
+        await full(dry, timeStamp, githubAccessToken, fetcher, options);
+    });
+
+    const log = loggerWithErrors()[0];
+    await fullOnce(log);
+    setInterval(fullOnce, 1_000_000, log);
+}
+
+// Even if there are many changes to DefinitelyTyped in a row, we only perform one update at a time.
+function updateOneAtATime(
+    doOnce: (log: LoggerWithErrors) => Promise<void>,
+): (log: LoggerWithErrors) => Promise<void> | undefined {
+    let working = false;
+    let anyUpdatesWhileWorking = false;
+
+    return (log) => {
+        if (working) {
+            anyUpdatesWhileWorking = true;
+            log.info("Not starting update, because already performing one.");
+            return undefined;
+        } else {
+            working = false;
+            anyUpdatesWhileWorking = false;
+            return work();
+        }
+
+        async function work(): Promise<void> {
+            log.info("Starting update");
+            working = true;
+            anyUpdatesWhileWorking = false;
+            do {
+                await doOnce(log);
+                working = false;
+            } while (anyUpdatesWhileWorking);
+        }
+    };
 }
