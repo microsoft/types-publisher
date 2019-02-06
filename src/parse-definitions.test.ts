0f96eb67c390f3d9289be753b3071c25d6870fbf
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-02-06 15:53:03 -0800

    Add a mock DT with jquery and angular

--- src/parse-definitions.test.ts
+++ src/parse-definitions.test.ts
@@ -1,5 +1,5 @@
 import parseDefinitions from "./parse-definitions";
-import { getDefinitelyTyped } from "./get-definitely-typed";
+import { Dir, getDefinitelyTyped, InMemoryDT } from "./get-definitely-typed";
 import { Options } from "./lib/common";
 import { loggerWithErrors } from "./util/logging";
 
@@ -7,6 +7,64 @@ function testo(o: { [s: string]: () => void }) {
     for (const k in o) {
         test(k, o[k], 100_000);
     }
+}
+function createMockDT() {
+    const root = new Dir(undefined);
+    root.set("notNeededPackages.json", `{
+    "packages": [{
+    "libraryName": "Angular 2",
+    "typingsPackageName": "angular",
+    "asOfVersion": "1.2.3",
+    "sourceRepoURL": "https://github.com/angular/angular2"
+  }]
+}`);
+    const types = root.subdir("types");
+    const jquery = types.subdir("jquery");
+    jquery.set("JQuery.d.ts", `
+declare var jQuery: 1;
+`);
+    jquery.set("index.d.ts", `// Type definitions for jquery 3.3
+// Project: https://jquery.com
+// Definitions by: Leonard Thieu <https://github.com/leonard-thieu>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+
+/// <reference path="JQuery.d.ts" />
+
+export = jQuery;
+`);
+    jquery.set("jquery-tests.ts", `
+console.log(jQuery);
+`);
+    jquery.set("tsconfig.json", `{
+    "compilerOptions": {
+        "module": "commonjs",
+        "lib": [
+            "es6",
+            "dom"
+        ],
+        "target": "es6",
+        "noImplicitAny": true,
+        "noImplicitThis": true,
+        "strictNullChecks": true,
+        "strictFunctionTypes": true,
+        "baseUrl": "../",
+        "typeRoots": [
+            "../"
+        ],
+        "types": [],
+        "noEmit": true,
+        "forceConsistentCasingInFileNames": true
+    },
+    "files": [
+        "index.d.ts",
+        "jquery-tests.ts"
+    ]
+}
+
+`);
+
+    return new InMemoryDT(root, "DefinitelyTyped");
 }
 testo({
     async parseDefinitions() {
@@ -22,4 +80,15 @@ testo({
         expect(j!.fullNpmName).toContain("jquery")
         expect(defs.allPackages().length).toEqual(defs.allTypings().length + defs.allNotNeeded().length)
     },
+    async mockParse() {
+        const log = loggerWithErrors()[0];
+        const defs = await parseDefinitions(createMockDT(), undefined, log);
+        expect(defs.allNotNeeded().length).toBe(1)
+        expect(defs.allTypings().length).toBe(1)
+        const j = defs.tryGetLatestVersion("jquery")
+        expect(j).toBeDefined()
+        expect(j!.fullNpmName).toContain("types")
+        expect(j!.fullNpmName).toContain("jquery")
+        expect(defs.allPackages().length).toEqual(defs.allTypings().length + defs.allNotNeeded().length)
+    }
 });
