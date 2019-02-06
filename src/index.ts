552317b51f12ab67bedff66be1c995776574e486
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-02-06 13:12:58 -0800

    Add exports for parsing Definitely Typed (#569)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+export { getDefinitelyTyped } from "./get-definitely-typed";
 export { CachedNpmInfoClient, NpmPublishClient, UncachedNpmInfoClient } from "./lib/npm-client";
 export { AllPackages } from "./lib/packages";
 export { getLatestTypingVersion } from "./lib/versions";
+export { default as parseDefinitions } from "./parse-definitions";
 
-export { consoleLogger } from "./util/logging";
+export { parseNProcesses } from "./tester/test-runner";
+export { consoleLogger, loggerWithErrors } from "./util/logging";
 export { logUncaughtErrors, nAtATime } from "./util/util";
 
 export { updateLatestTag, updateTypeScriptVersionTags } from "./lib/package-publisher";
+
