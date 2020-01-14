9dbc0b94b9addd1b1da9981ad3105eb8dc3cbfa2
Doug Brunner <doug@epicycloidengr.com>
2020-01-14 11:04:34 -0800

    Fixed assert on platforms with different sort rules (#728)

--- src/get-definitely-typed.ts
+++ src/get-definitely-typed.ts
@@ -225,7 +225,7 @@ class DiskFS implements FS {
     }
 
     readdir(dirPath?: string): ReadonlyArray<string> {
-        return assertSorted(readdirSync(this.getPath(dirPath))).filter(name => name !== ".DS_STORE");
+        return assertSorted(readdirSync(this.getPath(dirPath)).sort()).filter(name => name !== ".DS_STORE");
     }
 
     isDirectory(dirPath: string): boolean {
